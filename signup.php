1fa760242a07b16dc40f234488ac3b72c6b99549
henrysun918 <henrysun918@gmail.com>
2012-11-25 12:24:29 -0500

    signup page

--- signup.php
+++ signup.php
@@ -1,3 +1,44 @@
 <?php 
-	echo "lala!";
+	
+	
+	//connect to database
+	$dbc=mysqli_connect('localhost', 'root', 'testpassword', 'g_prototype') or die('Error connecting to MySQL server.');
+
+	{		
+		//extract profile from sign-up form
+		$username=$_POST['username'];
+		$password1=$_POST['password1'];
+		$password2=$_POST['password2'];
+
+
+		if(!empty($username) && !empty($password1) && !empty($password2) 
+		&& ($password1==$password2)){
+
+			//check uniqueness of username
+			$query="SELECT * FROM users WHERE username='$username'";
+
+			$data=mysqli_query($dbc, $query);
+			if(mysqli_num_rows($data)==0){
+				//username is unique
+				$query="INSERT INTO users (username, password, gene_count) VALUES ('$username', SHA('$password1'), '0')";
+
+				mysqli_query($dbc, $query) or die('Error insertion');
+
+				//confirm success
+				echo '<p>Sign up success!</p>';
+			
+				mysqli_close($dbc);
+				exit();
+			}
+			//username not unique
+			else{
+				echo '<p>Account already exists</p>';
+			}
+		}
+		//data invalid
+		else{
+			echo '<p>Invalid username or password</p>';
+		}
+	}
+	mysqli_close($dbc);
 ?>
