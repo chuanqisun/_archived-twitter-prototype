1be53eefd972a68cec2007df2602ad6588941473
henrysun918 <henrysun918@gmail.com>
2012-11-27 11:35:58 -0500

    individual banks functioning

--- newgene.php
+++ newgene.php
@@ -23,10 +23,67 @@
 
 	//if logged in and already posted
 	else if (isset($_POST['submit'])){
-		//parse and insert to database here
 		//Connect to database
 		require_once('connectvars.php');
 		$dbc=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Error connecting to MySQL Database');
+		
+		//parse geneinfo
+		$id=$_SESSION['id'];
+		$genename=$_POST['genename'];
+		$geneexpression=$_POST['geneexpression'];
+		
+		if(!empty($genename) && !empty($geneexpression)){
+				
+			//fix genes bank
+			$query="select * FROM genes WHERE name='$genename'";
+			$data=mysqli_query($dbc, $query);
+			$checkgene=mysqli_num_rows($data);
+			if ($checkgene == 0) {  //fresh gene
+
+				//insert into gene bank
+				$query="INSERT INTO genes (name, name_count, expression_count) VALUES ('$genename', '1', '1')";
+				mysqli_query($dbc, $query);
+			}else{  //gene already exists
+			echo "old gene in genes bank";	
+			
+				//increment in gene bank
+			}
+
+
+			//get gene id from gene bank
+			$query="SELECT * FROM genes WHERE name='$genename'";
+			$data=mysqli_query($dbc, $query);
+			$row=mysqli_fetch_array($data);
+			$gene_id=$row['id'];
+				
+
+			//fix gene's own table
+			if ($checkgene == 0) {  //create table when no current table exists
+				$query="CREATE TABLE gene_$gene_id (user_id int NOT NULL, expression text NOT NULL)";
+				mysqli_query($dbc, $query) or die('Error create gene\'s own table');
+			}
+
+			$query="INSERT INTO gene_$gene_id (user_id, expression) VALUES ('$id', '$geneexpression')";
+			mysqli_query($dbc, $query) or die('Error insert into gene\'s own talbe');
+
+			//fix user's individual gene bank
+			$query="SELECT * FROM user_$id WHERE name='$genename'";
+			$data=mysqli_query($dbc, $query);
+			$checkgene=mysqli_num_rows($data);
+			if ($checkgene == 0) { //fresh gene
+				//insert into user's own bank
+				$query="INSERT INTO user_$id (id, name, expression) VALUES ('$gene_id', '$genename', '$geneexpression')";
+				mysqli_query($dbc, $query);
+			}else{ //old gene
+				//update user's individual gene bank
+				echo "old gene in user's gene bank";
+			}
+
+			
+			
+		}else{
+			$error_msg= 'Sorry Please fill in both name and expression before adding';
+		} 
 	}
 
 	//print error
@@ -49,7 +106,3 @@
 	//insert footer
 	require_once('footer.php');
 ?>
-
-
-
-
--- signup.php
+++ signup.php
@@ -20,10 +20,18 @@
 
 			$data=mysqli_query($dbc, $query);
 			if(mysqli_num_rows($data)==0){
-				//username is unique
+				//username is unique, append to user talbe
 				$query="INSERT INTO users (username, password, gene_count) VALUES ('$username', SHA('$password1'), '0')";
+				mysqli_query($dbc, $query) or die('Error insertion into user table');
 
-				mysqli_query($dbc, $query) or die('Error insertion');
+				//get user id
+				$query="SELECT id FROM users WHERE username='$username'";
+				$row=mysqli_fetch_array(mysqli_query($dbc, $query));
+				$id=$row['id'];
+				
+				//create user's individual gene bank
+				$query="CREATE TABLE user_$id (gene_id int NOT NULL UNIQUE, name varchar(255) NOT NULL UNIQUE, expression text NOT NULL, PRIMARY KEY (id))";
+				mysqli_query($dbc, $query) or die('Error create user\'s gene bank');
 
 				//confirm success
 				echo '<p>Sign up success!</p>';
