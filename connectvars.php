6f84a8b17c553f52af6a1aac8b0cd83a698a87b3
henrysun918 <henrysun918@gmail.com>
2012-12-29 16:50:02 -0500

    avoid using root to connect sql and using remote

--- connectvars.php
+++ connectvars.php
@@ -1,8 +1,8 @@
 <?php
 
 	//Define Database Variables
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
+	define('DB_HOST', 'evenstar.dartmouth.edu');
+	define('DB_USER', 'g_user');
 	define('DB_PASSWORD', 'testpassword');
 	define('DB_NAME', 'g_prototype');
 ?>
