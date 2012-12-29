0f282465fdc3d82ffff21adc0f5902e3f182a00b
Chuanqi Sun <henrysun918@gmail.com>
2012-12-29 17:28:59 -0500

    switch back to local database

--- connectvars.php
+++ connectvars.php
@@ -1,7 +1,7 @@
 <?php
 
 	//Define Database Variables
-	define('DB_HOST', 'evenstar.dartmouth.edu');
+	define('DB_HOST', 'localhost');
 	define('DB_USER', 'g_user');
 	define('DB_PASSWORD', 'testpassword');
 	define('DB_NAME', 'g_prototype');
